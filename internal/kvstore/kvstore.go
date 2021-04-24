bcb00601104357dc70f16901c9b2c6cefc5453c0
Johan Nestaas <johannestaas@gmail.com>
2021-04-23 20:08:37 -0700

    fix printf

--- internal/kvstore/kvstore.go
+++ internal/kvstore/kvstore.go
@@ -8,13 +8,13 @@ type KVStore struct {
 }
 
 func (store *KVStore) Get(key string) string {
-	log.Printf("db %s getting key %s\n", store.name, key)
+	log.Printf("db %s: getting key %s\n", store.name, key)
 	result := store.memmap[key]
 	return result
 }
 
 func (store *KVStore) Set(key string, val string) {
-	log.Printf("db %s setting key %s to %s\n", store.name, key, val)
+	log.Printf("db %s: setting key %s to %s\n", store.name, key, val)
 	store.memmap[key] = val
 }
 
